5c4e5d8adc4818a5a3a0e7a3f0341318e1b32578
tatsenko-m <tatsenko.m@yandex.ru>
2023-04-17 22:35:10 +0300

    feat: добавить императивные обработчики для открытия попапов с формами

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
 function Main() {
+  
+  function handleEditAvatarClick() {
+    const popupWithUpdateAvatarForm = document.querySelector('.popup_act_update-avatar');
+    popupWithUpdateAvatarForm.classList.add('popup_opened');
+  }
+
+  function handleEditProfileClick() {
+    const popupWithEditProfileForm = document.querySelector('.popup_act_edit-profile');
+    popupWithEditProfileForm.classList.add('popup_opened');
+  }
+
+  function handleAddPlaceClick() {
+    const popupWithAddCardForm = document.querySelector('.popup_act_add-card');
+    popupWithAddCardForm.classList.add('popup_opened');
+  }
+  
   return (
     <main>
         <section className="profile">
@@ -9,14 +25,15 @@ function Main() {
               className="profile__avatar"
               src="/"
               alt="Аватар профиля"
+              onClick={handleEditAvatarClick}
             />
           </div>
           <article className="profile__info">
             <h1 className="profile__title"></h1>
             <p className="profile__subtitle"></p>
-            <button className="profile__edit-button" type="button"></button>
+            <button className="profile__edit-button" type="button" onClick={handleEditProfileClick}></button>
           </article>
-          <button className="profile__add-button" type="button"></button>
+          <button className="profile__add-button" type="button" onClick={handleAddPlaceClick}></button>
         </section>
         <section className="gallery">
           <ul className="gallery__list">
