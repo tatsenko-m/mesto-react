372fa1c971644b63e10dfba53f15f2f17a8e5d38
tatsenko-m <tatsenko.m@yandex.ru>
2023-04-18 14:20:34 +0300

    fix: исправить обработчик оверлея

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,23 +1,20 @@
-import React from "react";
+import React from 'react';
 
 function PopupWithForm({ title, name, children, isOpen, onClose }) {
   return (
     <div
-      className={`popup popup_type_${name}${isOpen ? " popup_opened" : ""}`}
+      className={`popup popup_type_${name}${isOpen ? ' popup_opened' : ''}`}
       onMouseDown={(evt) =>
-        evt.target.classList.contains("popup") &&
-        !evt.target.closest(".popup__container")
-          ? onClose()
-          : null
+        evt.target.classList.contains('popup_opened') ? onClose() : null
       }
     >
       <div
         className={`popup__container${
-          name === "confirmation"
-            ? " popup__container_size_s"
-            : name === "avatar"
-            ? " popup__container_size_m"
-            : ""
+          name === 'confirmation'
+            ? ' popup__container_size_s'
+            : name === 'avatar'
+            ? ' popup__container_size_m'
+            : ''
         }`}
       >
         <button
@@ -29,11 +26,11 @@ function PopupWithForm({ title, name, children, isOpen, onClose }) {
           <h2 className="popup__title">{title}</h2>
           {children}
           <button className="popup__save-button" type="submit">
-            {name === "confirmation"
-              ? "Да"
-              : name === "card"
-              ? "Создать"
-              : "Сохранить"}
+            {name === 'confirmation'
+              ? 'Да'
+              : name === 'card'
+              ? 'Создать'
+              : 'Сохранить'}
           </button>
         </form>
       </div>
