79c0f2a2b78a046f7cb674bcc9c7a8e0e50af4d3
tatsenko-m <tatsenko.m@yandex.ru>
2023-05-01 21:09:15 +0300

    feat: сделать компонент EditProfilePopup управляемым

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -2,6 +2,21 @@ import React from 'react';
 import PopupWithForm from './PopupWithForm';
 
 function EditProfilePopup({ isOpen, onClose }) {
+  const [name, setName] = React.useState('');
+  const [description, setDescription] = React.useState('');
+
+  function handleInputChange(evt) {
+    const target = evt.target;
+    const value = target.value;
+    const name = target.name;
+
+    if (name === 'name') {
+        setName(value);
+      } else if (name === 'about') {
+        setDescription(value);
+      }
+  }
+
   return (
     <PopupWithForm 
       title="Редактировать профиль" 
@@ -14,6 +29,8 @@ function EditProfilePopup({ isOpen, onClose }) {
         id="name"
         className="popup__item popup__item_type_name"
         type="text"
+        value={name}
+        onChange={handleInputChange}
         placeholder="Имя"
         minLength="2"
         maxLength="40"
@@ -25,6 +42,8 @@ function EditProfilePopup({ isOpen, onClose }) {
         id="about"
         className="popup__item popup__item_type_about"
         type="text"
+        value={description}
+        onChange={handleInputChange}
         placeholder="О себе"
         minLength="2"
         maxLength="200"
