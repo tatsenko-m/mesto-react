cb0c2a5d242e08caf360c9af625fa7d662fff7fa
tatsenko-m <tatsenko.m@yandex.ru>
2023-05-01 14:03:52 +0300

    feat: подписать на контекст CurrentUserContext компонент Card и реализовать динамическую отрисовку кнопок лайка и удаления

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Card(props) {
-  
+  const currentUser = React.useContext(CurrentUserContext);
+
+  const isOwn = props.card.owner._id === currentUser._id;
+  const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+
+  const cardLikeButtonClassName = ( 
+    `card__like-button ${isLiked && 'card__like-button_active'}` 
+  );
+
   function handleClick() {
    props.onCardClick(props.card);
   }
@@ -12,11 +21,11 @@ function Card(props) {
       <div className="card__description">
         <h2 className="card__title">{props.card.name}</h2>
         <div className="card__like-group">
-          <button className="card__like-button" type="button"></button>
+          <button className={cardLikeButtonClassName} type="button" />
           <span className="card__like-counter">{props.card.likes.length}</span>
         </div>
       </div>
-      <button className="card__delete-button" type="button"></button>
+      {isOwn && <button className="card__delete-button" type="button" />}
     </li>
   );
 }
